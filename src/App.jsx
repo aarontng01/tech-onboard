c2b8ca59ed0dda002c9c5dc98af3738c55afdadf
Aaron Tng <aarontng1@gmail.com>
2024-05-31 11:50:08 -0700

    ''

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aaron Tng",
+  title: "Microsoft Ceritified Cybersecurity Architect",
+  email: "aarontng1@gmail.com",
+  gitHub: "https://github.com/aarontng01/",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/aaron-tng-80825722a/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
