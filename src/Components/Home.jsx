108417700aed5289eb4fede0b701510342591569
Aaron Tng <aarontng1@gmail.com>
2024-06-01 18:29:20 -0700

    n/a

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/bluehat-aud.JPEG";
+import image from "../images/bluehat-aud.png";
 
 const imageAltText = "bluehat auditorium";
 
