faa348fcfa7725f8999f6364bfd5e29835a32378
Aaron Tng <aarontng1@gmail.com>
2024-06-01 18:46:28 -0700

    n/a

---
+++ src/Components/Education.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Education = () => {
+  return (
+    <section id="education" className="min-height">
+      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+        <h1>Education</h1>
+        <h2>Microsoft Certified Cybersecurity Architect</h2>
+      </div>
+    </section>
+  );
+}
